38240a657ca74c6c869ec4c6162b167b84021240
DumbGameMaker <cael.thebad+github@gmail.com>
2020-07-17 14:59:51 -0700

    write stuffz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DumbThings from "./DumbThings.js"
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top">
+        <h1 id="title">Dumb Game Maker Made this Dumb Game!</h1>
+        <p>The only game where you do dumb things to make a number big!<br/>
+        Really, don't play this game! It's dumb and made by the dumb game maker<br/>
+        What's that? You want to play? NO! DON'T PLAY THE DUMB GAME!<br/>
+        <i>oh, and the buttons are below.</i></p>
+      </div>
+    
+      <DumbThings />
+      <footer>
+        Note: this game is a joke and will NOT download or install anything. <br />
+        Suggestions, Issues, or improvements? Check out our <a href="https://github.com/DumbGameMaker/MadeThisDumbGame">Github!</a><br />
+        DISCLAIMER: <br />
+        There will be times when the game asks you for personal info, do NOT under ANY CIRCUIMSTANCES give it your personal info <br />
+        Even though the game doesn't process it, it's good practice NOT to give random games your info. I am not responsible if <br />
+        your accounts or computer gets hacked because you did. Although... the point of the game IS to do dumb stuff so... 
+        <br />
+        Made with love by DumbGameMaker<br />
+        <br />
+        Licenced under the CC BY-SA-NC licence. <br />
+        TL/DR you can do whatever you want with this software, for free, <br />
+        but if you share this software or any modifications of it you MUST share <br />
+        under the same licence AND make a total of exactly $0.00 off of it.<br />
+        <br />
+      </footer>
     </div>
   );
 }
